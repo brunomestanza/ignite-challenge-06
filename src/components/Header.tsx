761a0fb77cdbda1bfdcc49c466d7c07fc8b5e78d
Bruno Mestanza <brunommestanza@gmail.com>
2022-04-06 17:21:22 -0300

    Feat: Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Flex, Icon, Image } from "@chakra-ui/react";
+import { IoIosArrowBack } from "react-icons/io";
+
+export function Header() {
+  return (
+    <Flex as="header" py="7" maxWidth={1440} mx="auto">
+			{/* <Icon alignSelf="center" fontSize={24} as={IoIosArrowBack} ml="144" /> */}
+			<Image alt="World trip logo" src="/logo.png" mx="auto" />
+    </Flex>
+  );
+};
